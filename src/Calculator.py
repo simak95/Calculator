060eba28a00d1ccfbcd18920dd15324e1deb9475
simak95 <sk724@njit.edu>
2021-04-18 23:54:14 -0400

    Task: added calcs

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,12 +1,33 @@
+from src.Subtraction import Subtraction
+from Calculator.Addition import
+
 class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 + 2
-        self.result = x;
-
-    def subtraction(x, y):
-        return x - y
         pass
 
+    def add(self, x, y):
+        self.result = addition(x, y)
+        return self.result
+
+    def subtract(self, x, y):
+        self.result = subtraction(x, y)
+        return self.result
+
+    def multiply(self, x, y):
+        self.result = multiplication(x, y)
+        return self.result
+
+    def divide(self, x, y):
+        self.result = division(x, y)
+        return self.result
+
+    def square(self, x):
+        self.result = square(x)
+        return self.result
+
+    def square_root(self, x):
+        self.result = square_root(x)
+        return self.result
 
